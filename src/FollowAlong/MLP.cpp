c569b20636f7cd2e7706b4d33cf592946dc3cdf0
Nicholas Luis <nml5604@psu.edu>
2024-07-25 01:16:05 +0000

    Created a folder that will have all my edits as I follow along with the lessons

---
+++ src/FollowAlong/MLP.cpp
@@ -0,0 +1,39 @@
+#include "MLP.h"
+// Last updated: Ch2, L5
+
+double frand(){
+	return (2.0*(double)rand() / RAND_MAX) - 1.0;
+}
+
+
+// Return a new Perceptron object with the specified number of inputs (+1 for the bias).
+Perceptron::Perceptron(size_t inputs, double bias){
+	this->bias = bias;
+	weights.resize(inputs+1);
+	generate(weights.begin(),weights.end(),frand);
+}
+
+// Run the perceptron. x is a vector with the input values.
+double Perceptron::run(std::vector<double> x){
+	x.push_back(bias);
+	double sum = inner_product(x.begin(),x.end(),weights.begin(),(double)0.0);
+	return sigmoid(sum);
+}
+
+// Challenge: Finish the following functions:
+
+void Perceptron::set_weights(std::vector<double> w_init){
+	// Verifies that this is same size as inputs + 1 (activation fcn)
+	if (weights.size() != w_init.size()) {
+		std::cout << "Incorrect w_init input size" << std::endl;
+	}
+	else {
+		weights = w_init;
+	}
+	
+}
+
+double Perceptron::sigmoid(double x){
+	// Return the output of the sigmoid function applied to x
+	return 1 / (1 + exp(-x));
+}
