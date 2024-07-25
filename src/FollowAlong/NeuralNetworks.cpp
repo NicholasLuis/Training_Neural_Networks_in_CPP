252bea2706560d72c8295cd5a93c4d6cd80a3608
Nicholas Luis <nml5604@psu.edu>
2024-07-25 01:39:02 +0000

    My solution to creating an OR gate using a Perceptron

--- src/FollowAlong/NeuralNetworks.cpp
+++ src/FollowAlong/NeuralNetworks.cpp
@@ -1,8 +1,27 @@
 // NeuralNetworks.cpp : This file contains the 'main' function. Program execution begins and ends there.//
 
+// Last updated: Ch2, L8
+
 #include "MLP.h"
 
 int main(){
     srand(time(NULL));
     rand();
+
+    std::cout << "\n\n--------Logic Gate Example----------------\n\n";
+    Perceptron p(2);
+
+    p.set_weights({10,10,-15}); //AND
+    std::cout << "And Gate" << std::endl;
+    std::cout << p.run({0,0}) << std::endl;
+    std::cout << p.run({0,1}) << std::endl;
+    std::cout << p.run({1,0}) << std::endl;
+    std::cout << p.run({1,1}) << std::endl;
+
+    p.set_weights({20,20,-10}); //OR
+    std::cout << "OR Gate" << std::endl;
+    std::cout << p.run({0,0}) << std::endl;
+    std::cout << p.run({0,1}) << std::endl;
+    std::cout << p.run({1,0}) << std::endl;
+    std::cout << p.run({1,1}) << std::endl;
 }
